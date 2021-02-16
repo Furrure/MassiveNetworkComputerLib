6e22aad0046a9fc9d187bdf7a26e8183f6248fe3
Furrure <71794171+Furrure@users.noreply.github.com>
2021-02-16 09:29:12 -0500

    Changed organization structure

---
+++ Source/Windows/MNC.c
@@ -0,0 +1,17 @@
+#include <socket.h>
+// Library for creating and making use of Massive Network Computer Instances
+
+typedef struct {
+	char IP;
+	int PORT;
+	int sockid;
+} MNC_instance;
+
+int init(MNC_instance instance) {
+	
+}
+
+
+int main() {
+
+}
